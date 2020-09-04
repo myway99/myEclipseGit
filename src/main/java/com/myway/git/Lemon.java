aa52dd906d28768ab0276878ae0e20be4f1363bc
myway <myway99@sina.com>
2020-09-05 04:16:38 +0800

    commit ok

--- src/main/java/com/myway/git/Lemon.java
+++ src/main/java/com/myway/git/Lemon.java
@@ -6,20 +6,15 @@ public class Lemon {
   
   public static void main(String[] args) {
     
-<<<<<<< HEAD
 
-=======
     System.out.println("Test_r ... ");
->>>>>>> branch 'master' of https://github.com/myway99/myEclipseGit.git
+
     System.out.println("Test_l ... ");
-<<<<<<< HEAD
 
     System.out.println("Test_r ... ");
-
-    
-=======
+ 
     System.out.println("hot fix ... ");
->>>>>>> branch 'master' of https://github.com/myway99/myEclipseGit.git
+
   }
 
 }
